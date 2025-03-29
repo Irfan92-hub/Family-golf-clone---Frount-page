587550ab8337d38ccb1ee0535e7d19c41ec657dc
Irfan <9667686088s@gmail.com>
2025-03-29 16:20:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+gsap.to("#nav",{
+    backgroundColor: "black",
+    duration: 0.5,
+    height: "120px",
+    ScrollTigger:{
+        trigger :"#nav",
+        Scroller: "body",
+        markers: true,
+        start :" top -10px",
+
+    }
+ 
+}) 
